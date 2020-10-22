cb8160fa229ea0943e1559a69cb7c531c9c96937
Rory Liberty <roryliberty@gmail.com>
2020-10-22 17:09:10 -0500

    Start Over button is working to an extent. Need to work on the size of the grid-container

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 const gridContainer = document.getElementById("grid-container");
+const startOverButton = document.getElementById("start-over");
 
 
-function makeGrid(rows, columns) {
-    for (i = 0; i < (rows * columns); i++) {
+function makeGrid(rows) {
+    for (i = 0; i < (rows * rows); i++) {
         gridContainer.style.setProperty("--grid-rows", rows)
-        gridContainer.style.setProperty("--grid-cols", columns);
+        gridContainer.style.setProperty("--grid-cols", rows);
         let cell = document.createElement("div");
         cell.id = (i + 1);
         cell.innerHTML = cell.id;
         gridContainer.appendChild(cell).className = "grid-cell";
         
+        const boxes = gridContainer.querySelectorAll(".grid-cell");
+
+        boxes.forEach((div) => {
+            div.addEventListener("mouseover", (e) => {
+                console.log(e);
+                let boxNumber = e.target.id;
+                div.style.backgroundColor = "black";
+            });
+        });
     }
 }
 
+makeGrid(16);
 
 
-makeGrid(16, 16);
 
+function clearGrid() {
+    gridContainer.innerHTML = "";
+    let numRowsCols = prompt("How many rows/columns (max 100)?", 16);
+    if (numRowsCols > 100 || numRowsCols === null) {
+        numRowsCols = 16;
+    }
 
-const boxes = gridContainer.querySelectorAll(".grid-cell");
-
-boxes.forEach((div) => {
-    div.addEventListener("mouseover", (e) => {
-        console.log(e);
-        let boxNumber = e.target.id;
-        div.style.backgroundColor = "black";
-    });
-});
+    makeGrid(numRowsCols)
+}
